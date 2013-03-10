8a0c6404641d4ffd937ae35ace04683d38b88d06
Alex Gray <alex@mrgray.com>
2013-03-09 19:41:41 -0500

    fuck python!

---
+++ VideoIO/SIPRunner.h
@@ -0,0 +1,13 @@
+//
+//  SIPRunner.h
+//  VideoIO
+//
+//  Created by Alex Gray on 09/03/2013.
+//  Copyright (c) 2013 Alex Gray. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SIPRunner : NSOperation
+
+@end
---
+++ VideoIO/SIPRunner.m
@@ -0,0 +1,13 @@
+//
+//  SIPRunner.m
+//  VideoIO
+//
+//  Created by Alex Gray on 09/03/2013.
+//  Copyright (c) 2013 Alex Gray. All rights reserved.
+//
+
+#import "SIPRunner.h"
+
+@implementation SIPRunner
+
+@end
